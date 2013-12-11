f50e9c83815ecfbcb95176aad4064881bbda3d08
Kirk Tarou <kgtarou@gmail.com>
2013-12-11 13:49:42 -0800

    On Collection page, click URL to see seed detail Click pencil on Collection detail page and Seed detail page to edit name

--- app/controllers/SeedController.java
+++ app/controllers/SeedController.java
@@ -21,6 +21,8 @@ package controllers;
 
 
 
+import java.util.Map;
+
 import javax.persistence.metamodel.PluralAttribute.CollectionType;
 
 import com.avaje.ebean.Ebean;
@@ -29,6 +31,7 @@ import models.ArchiveItCollection;
 import models.Seed;
 import models.SeedComment;
 import models.SeedType;
+import play.data.DynamicForm;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -123,6 +126,22 @@ public class SeedController extends Controller
 
 	}
 
+	public static Result update2()
+	{
+		DynamicForm request = Form.form().bindFromRequest();
+		System.out.println(request.toString());
+		Map<String, String> data = request.data();
+		String pk = request.get("pk");
+		System.out.println("pk: " + pk);
+		String name = request.get("value");
+		System.out.println("name: " + name);
+
+		Seed s = Seed.find.byId(Long.parseLong(pk));
+		s.setUrl(name);
+		Ebean.save(s);
+		return ok();
+
+	}
 	public static Result addComment(Long seedId)
 	{
 		SeedComment c = Form.form(SeedComment.class).bindFromRequest().get();
